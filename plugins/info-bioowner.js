2f86a5c97bce603439ffc965f10b4034df107040
Gerall09 <103030918+Gerall09@users.noreply.github.com>
2022-07-14 17:25:22 +0800

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,24 +1,24 @@
 let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*💌 Nama* : ᴅᴀɴɪ-ʙᴏᴛᴢ
-*✉️ Nama RL* : ᴅᴀɴɪ ᴏғғᴄɪᴀʟ
+*💌 Nama* : Gerall
+*✉️ Nama RL* : Yo Nd Tau
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 16 Desember 2007
-*🎨 Umur* : 15
-*🧮 Kelas* : 9
+*⏰ Tanggal lahir* : 7 Desember 2005
+*🎨 Umur* : 17
+*🧮 Kelas* : 11
 *🧩 Hobby* : Nonton Hanime, Chatting, Recode script bot
 *💬 Sifat* : Idiot, Tidak Ramah, Kanjut, Prik, Pedofil
-*🗺️ Tinggal* : Indo, Kalsel, Kandangan, Hss
-*❤️ Suka* : warnah kuning & biru, Hanime, waifu, loli, trap, furry, kucing
+*🗺️ Tinggal* : Indo, SulSel, Makassar, MkS
+*❤️ Suka* : warnah hitam(shap), Hanime, waifu, loli, trap, furry, kucing
 *💔 Benci* : autis, anak epep, seleb
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @Dani_Offcial
-*🇫  Facebook* : Dani_Offc
-*🏮 Chanel Youtube* : DaniBotz
-*🐈 Github:* YTDANIBOTZ
+*📷 instagran* : rall_nation09
+*🇫  Facebook* : Gerall
+*🏮 Chanel Youtube* : AlmGrll
+*🐈 Github:* Gerall09
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
